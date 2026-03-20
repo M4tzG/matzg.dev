e79889b66d1168ea4da97f621dea9865ca3f8bda
Matheus Gabriel <99271709+M4tzG@users.noreply.github.com>
2026-03-20 10:52:52 -0300

    locale

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang={process.env.LOCALE || 'pt-BR'}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} flex justify-center antialiased`}
       >
