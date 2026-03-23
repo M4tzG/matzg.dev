195c57713af5e1e40650c734c1756de9269ca10a
Matheus Gabriel <99271709+M4tzG@users.noreply.github.com>
2026-03-23 11:55:25 -0300

    Add basePath support and prefix asset URLs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/matzg.dev' : '';
+
 const nextConfig = {
-  /* config options here */
   output: 'export',
   reactCompiler: true,
+
+  basePath: basePath,
+
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
 };
 
 export default nextConfig;
